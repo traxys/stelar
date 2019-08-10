9b8ed444d1a1eaa2b82f311418b6cdd3490566ec
Quentin Boyer <qbsecond@gmail.com>
2019-08-10 17:59:16 +0200

    Context

--- stelar/src/lib.rs
+++ stelar/src/lib.rs
@@ -18,35 +18,41 @@ pub struct ValuedToken<T, V> {
 ///! Trait to extract values out of a input storage
 pub trait Extract<I>: Sized {
     type Value;
+    type Context;
 
-    fn extract(input: &mut I) -> Option<ValuedToken<Self, Self::Value>>;
+    fn extract(
+        input: &mut I,
+        context: &mut Self::Context,
+    ) -> Option<ValuedToken<Self, Self::Value>>;
 }
 
 ///! Tokens over an input storage
-pub struct TokenStream<I, T>
+pub struct TokenStream<I, T, C>
 where
-    T: Extract<I>,
+    T: Extract<I, Context = C>,
 {
     input: I,
+    context: C,
     _marker: std::marker::PhantomData<T>,
 }
 
-impl<T: Extract<I>, I> TokenStream<I, T> {
-    pub fn new(input: I) -> TokenStream<I, T> {
+impl<T: Extract<I, Context = C>, I, C> TokenStream<I, T, C> {
+    pub fn new(input: I, context: C) -> TokenStream<I, T, C> {
         TokenStream {
+            context,
             input,
             _marker: std::marker::PhantomData,
         }
     }
 }
 
-impl<I, T> Iterator for TokenStream<I, T>
+impl<I, T, C> Iterator for TokenStream<I, T, C>
 where
-    T: Extract<I>,
+    T: Extract<I, Context = C>,
 {
     type Item = ValuedToken<T, T::Value>;
     fn next(&mut self) -> Option<Self::Item> {
-        T::extract(&mut self.input)
+        T::extract(&mut self.input, &mut self.context)
     }
 }
 
